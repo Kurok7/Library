49d9964cc7da5b3ca59e90c83148673c4510ba92
Kurok7 <malonejuank@gmail.com>
2022-09-19 17:21:02 -0700

    Added html and js files

---
+++ main.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Books(title, author, pages, haveRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.haveRead = haveRead
+    this.info = function() {
+        return `${title} by ${author} has ${pages} and I think i might have read it (${haveRead})`
+    }
+}
+
+const book1 = new Books('1984', 'George Orwell', 200, false);
+const book2 = new Books('Discrimination and Disparities', 'Thomas Sowell', 280, true);
+const book3 = new Books('The Mis-Education of the Negro', 'Carter G. Woodson', 210, true);
