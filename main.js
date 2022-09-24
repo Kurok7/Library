3eb7807686d2f7f3bb6bcb5579fae59ee8887fee
Kurok7 <malonejuank@gmail.com>
2022-09-23 18:48:04 -0700

    Added a function

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ function Books(title, author, pages, haveRead) {
 const book1 = new Books('1984', 'George Orwell', 200, false);
 const book2 = new Books('Discrimination and Disparities', 'Thomas Sowell', 280, true);
 const book3 = new Books('The Mis-Education of the Negro', 'Carter G. Woodson', 210, true);
+
+/*              Iterates over myLibrary
+function iterateObject() {
+    for (let key in myLibrary) {
+        if (myLibrary.hasOwnProperty(key)) {
+            value = myLibrary[key];
+            console.log(key, value);
+        }
+    }
+} */
